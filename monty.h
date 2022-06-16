a699b9b4ebed436d4510c99a177c2fc8b948e0fb
kelvintrc <kvynmutwiri@gmail.com>
2022-06-16 07:16:29 +0000

    [Feature]: Implementing the pop opcode

--- monty.h
+++ monty.h
@@ -61,6 +61,7 @@ void get_op(char *op, stack_t **stack, unsigned int line_number);
 void m_push(stack_t **stack, unsigned int line_number);
 void m_pall(stack_t **stack, unsigned int line_number);
 void m_pint(stack_t **stack, unsigned int line_number);
+void m_pop(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
