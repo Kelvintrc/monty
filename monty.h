1576b8280e42fa8e0b4b13757ef700f0b8619e5e
kelvintrc <kvynmutwiri@gmail.com>
2022-06-16 09:12:49 +0000

    [Feature]: Implementing the div opcode

--- monty.h
+++ monty.h
@@ -69,6 +69,7 @@ void m_swap(stack_t **stack, unsigned int line_number);
 void m_add(stack_t **stack, unsigned int line_number);
 void m_nop(stack_t **stack, unsigned int line_number);
 void m_sub(stack_t **stack, unsigned int line_number);
+void m_div(stack_t **stack, unsigned int line_number);
 void free_stack(int status, void *arg);
 void m_fs_close(int status, void *arg);
 void free_lineptr(int status, void *arg);
