eec31682f5d661f16b26670a783046b5c03806c9
kelvintrc <kvynmutwiri@gmail.com>
2022-06-16 10:26:55 +0000

    [Update]: Updated monty.h

--- monty.h
+++ monty.h
@@ -63,6 +63,7 @@ var_t var;
 
 void get_op(char *op, stack_t **stack, unsigned int line_number);
 void m_push(stack_t **stack, unsigned int line_number);
+void m_push2(stack_t **stack, int n);
 void m_pall(stack_t **stack, unsigned int line_number);
 void m_pint(stack_t **stack, unsigned int line_number);
 void m_pop(stack_t **stack, unsigned int line_number);
