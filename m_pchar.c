85402a8a41ea30ce2ba27ab84693b32972e06917
kelvintrc <kvynmutwiri@gmail.com>
2022-06-18 12:26:07 +0000

    [Fix]: Printing the error message to STDERR

--- m_pchar.c
+++ m_pchar.c
@@ -14,7 +14,7 @@ void m_pchar(stack_t **stack, unsigned int line_number)
 
 	if (var.stack_len < 1)
 	{
-		dprintf(STDOUT_FILENO,
+		dprintf(STDERR_FILENO,
 				"L%u: can't pchar, stack empty\n",
 				line_number);
 		exit(EXIT_FAILURE);
