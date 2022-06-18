11bc77401b3396849523ac898b58908f8cf3bf66
kelvintrc <kvynmutwiri@gmail.com>
2022-06-18 13:02:49 +0000

    [Fix]: Moving the global variable from main.h to monty.h

--- monty.h
+++ monty.h
@@ -58,6 +58,9 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/*global variable for the main function*/
+var_t var;
+
 void get_op(char *op, stack_t **stack, unsigned int line_number);
 void m_push(stack_t **stack, unsigned int line_number);
 void m_pall(stack_t **stack, unsigned int line_number);
